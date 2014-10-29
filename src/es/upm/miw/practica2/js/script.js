a30c07a398c76078b17f3fc2e355e6932941facf
guille209 <gguillemiranda@gmail.com>
2014-10-29 13:53:52 +0100

    Practica Jquery

--- src/es/upm/miw/practica2/js/script.js
+++ src/es/upm/miw/practica2/js/script.js
@@ -1,4 +1,12 @@
-
-function cargarFecha(){
-	document.getElementById("fechaEmision").value = new Date().toLocaleDateString("es-ES");
+var fechaActual = new Date();
+function cargarFecha() {
+	document.getElementById("fechaEmision").value = fechaActual
+			.toLocaleDateString("es-ES");
+}
+function calcularFecha(radiobutton) {
+	var fechaCalculada = new Date();
+	fechaCalculada.setMilliseconds(fechaActual.getMilliseconds()
+			+ (radiobutton.value) * 24 * 60 * 60 * 1000);
+	document.getElementById("fechaLimite").value = fechaCalculada
+			.toLocaleDateString("es-ES")
 }
