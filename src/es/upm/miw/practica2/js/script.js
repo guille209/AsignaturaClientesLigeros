217437e095c44e4848a15456a14cb4b2df8e9882
guille209 <gguillemiranda@gmail.com>
2014-10-30 13:42:38 +0100

    onkeyup cell elements

--- src/es/upm/miw/practica2/js/script.js
+++ src/es/upm/miw/practica2/js/script.js
@@ -9,42 +9,66 @@ function calcularFecha(radiobutton) {
 	fechaCalculada.setMilliseconds(fechaActual.getMilliseconds()
 			+ (radiobutton.value) * 24 * 60 * 60 * 1000);
 	document.getElementById("fechaLimite").value = fechaCalculada
-			.toLocaleDateString("es-ES")
+			.toLocaleDateString("es-ES");
 }
 
 function aniadirConcepto() {	
 	var table = document.getElementById("myTable");
 	if (table.rows.length < 9) {
 		var row = table.insertRow(table.rows.length);
-		for (var i = 0; i < 4; i++) {
-			if (i != 3) {
+		for (var i = 0; i <= 4; i++) {
+			if (i < 3) {
 				row.insertCell(i).contentEditable = true;
-			} else {
+				//Importe
+			} else if(i==3){
 				row.insertCell(i).className = "importe";
+				//Eliminar
+			}else{
+				row.insertCell(i).innerHTML = "<button class=\"btnEliminar\" type=\"button\" onclick=\"eliminarConcepto(this)\">x</button>";
 			}
 		}
 	}
+	table.rows[table.rows.length-1].cells[1].setAttribute('onkeyup',"calcularDatos(this)");
+	table.rows[table.rows.length-1].cells[2].setAttribute('onkeyup',"calcularDatos(this)");
+		comprobarNumeroConceptos();
+}
+function eliminarConcepto(miBoton) {
+	miBoton.parentNode.parentNode.remove();
 	comprobarNumeroConceptos();
 }
-function eliminarConcepto() {
+
+function eliminarTodosConcepto(){
 	var table = document.getElementById("myTable");
 	if (table.rows.length > 2) {
-		table.rows[table.rows.length - 1].remove();
+		for(var index=table.rows.length-1;index>=2;index--){
+			table.deleteRow(index);
+		}
 	}
 	comprobarNumeroConceptos();
 }
 
 function comprobarNumeroConceptos() {
+	//activa o desactiva los botones de eliminar y/o aniadir
 	var table = document.getElementById("myTable");
 	if (table.rows.length > 2) {
-		document.getElementById("btnEliminar").disabled = false;
+		document.getElementById("btnEliminarTodo").disabled = false;
 	}else {
-		document.getElementById("btnEliminar").disabled = true;
+		document.getElementById("btnEliminarTodo").disabled = true;
 	}
 	if(table.rows.length == 8){
 		document.getElementById("btnAniadir").disabled = false;
 	}
 	if(table.rows.length == 9){
 		document.getElementById("btnAniadir").disabled = true;
+	}
+}
+
+function calcularDatos(miTd){
+	var precioUnidad = miTd.parentNode.cells[1].innerHTML;
+	var cantidad = miTd.parentNode.cells[2].innerHTML;
+	if(precioUnidad!=""&&cantidad!=""){
+		alert("calculardatos");
+		miTd.parentNode.cells[3].innerHTML = parseInt(precioUnidad) * parseInt(cantidad);
+		
 	}
 }
