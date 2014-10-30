d7fa5a35b8dc2ffb02d5597ec6397f7bf6d43eb2
guille209 <gguillemiranda@gmail.com>
2014-10-30 10:23:32 +0100

    Aniadida tabla

--- src/es/upm/miw/practica2/js/script.js
+++ src/es/upm/miw/practica2/js/script.js
@@ -1,5 +1,6 @@
 var fechaActual = new Date();
-function cargarFecha() {
+function cargarDatosIniciales() {
+	// Fecha emision
 	document.getElementById("fechaEmision").value = fechaActual
 			.toLocaleDateString("es-ES");
 }
@@ -9,4 +10,41 @@ function calcularFecha(radiobutton) {
 			+ (radiobutton.value) * 24 * 60 * 60 * 1000);
 	document.getElementById("fechaLimite").value = fechaCalculada
 			.toLocaleDateString("es-ES")
+}
+
+function aniadirConcepto() {	
+	var table = document.getElementById("myTable");
+	if (table.rows.length < 9) {
+		var row = table.insertRow(table.rows.length);
+		for (var i = 0; i < 4; i++) {
+			if (i != 3) {
+				row.insertCell(i).contentEditable = true;
+			} else {
+				row.insertCell(i).className = "importe";
+			}
+		}
+	}
+	comprobarNumeroConceptos();
+}
+function eliminarConcepto() {
+	var table = document.getElementById("myTable");
+	if (table.rows.length > 2) {
+		table.rows[table.rows.length - 1].remove();
+	}
+	comprobarNumeroConceptos();
+}
+
+function comprobarNumeroConceptos() {
+	var table = document.getElementById("myTable");
+	if (table.rows.length > 2) {
+		document.getElementById("btnEliminar").disabled = false;
+	}else {
+		document.getElementById("btnEliminar").disabled = true;
+	}
+	if(table.rows.length == 8){
+		document.getElementById("btnAniadir").disabled = false;
+	}
+	if(table.rows.length == 9){
+		document.getElementById("btnAniadir").disabled = true;
+	}
 }
